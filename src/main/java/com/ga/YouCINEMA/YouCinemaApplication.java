b93320ed2efd3b734f7f67873a56e830719e302d
yousif <yousif.sei07@gmail.com>
2026-03-30 20:55:28 +0300

    project setup added the dependenices , folder strcture , spring profies

---
+++ src/main/java/com/ga/YouCINEMA/YouCinemaApplication.java
@@ -0,0 +1,13 @@
+package com.ga.YouCINEMA;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YouCinemaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YouCinemaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ga/YouCINEMA/YouCinemaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ga.YouCINEMA;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YouCinemaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
