6f0ba8135224d414b76158b17bff5df192afb106
yousif <yousif.sei07@gmail.com>
2026-03-31 18:50:38 +0300

    add the user model

---
+++ src/main/java/com/ga/YouCINEMA/model/User.java
@@ -0,0 +1,32 @@
+package com.ga.YouCINEMA.model;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
+public class User {
+
+    private Long id;
+
+    private String firstName;
+
+    private String lastName;
+
+    private String email;
+
+    private  String password;
+
+    private UserRole role;
+
+    private UserStatus status;
+
+    private String profilePicture;
+
+    private boolean emailVerified = false;
+
+    private LocalDateTime createdAt;
+
+    private LocalDateTime updatedAt;
+
+    private LocalDateTime deletedAt;
+
+}
