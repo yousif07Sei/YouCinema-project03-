5e45adbe2736df53cc9e2dc12e7e9efadb49962c
yousif <yousif.sei07@gmail.com>
2026-03-31 19:07:46 +0300

    add the Movie model

---
+++ src/main/java/com/ga/YouCINEMA/model/Movie.java
@@ -0,0 +1,27 @@
+package com.ga.YouCINEMA.model;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+
+public class Movie {
+    private Long id ;
+
+    private String title;
+
+    private String description;
+
+    private int duration;
+
+    private String genre;
+
+    private String language;
+
+    private String posterUrl;
+
+    private LocalDate releaseDate;
+
+    private LocalDateTime createdAt;
+
+    private LocalDateTime updatedAt;
+
+}
