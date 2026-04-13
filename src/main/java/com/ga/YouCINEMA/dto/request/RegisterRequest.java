794e5f24c2d7a8c86bea9592a0fb51c67a702036
yousif <yousif.sei07@gmail.com>
2026-04-13 18:55:31 +0300

    add the dto RegisterRequest

---
+++ src/main/java/com/ga/YouCINEMA/dto/request/RegisterRequest.java
@@ -0,0 +1,26 @@
+package com.ga.YouCINEMA.dto.request;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import lombok.*;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class RegisterRequest {
+
+    @NotBlank(message = "First name is required")
+    private String firstName;
+
+    @NotBlank(message = "Last name is required")
+    private String lastName;
+
+    @Email(message = "Email must be valid")
+    @NotBlank(message = "Email is required")
+    private String email;
+
+    @NotBlank(message = "Password is required")
+    private String password;
+}
