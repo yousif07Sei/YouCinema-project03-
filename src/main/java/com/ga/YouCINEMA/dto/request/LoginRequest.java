2f0fa9191408e7a5e4ac20e991b3c4c34014069c
yousif <yousif.sei07@gmail.com>
2026-04-13 18:58:50 +0300

    add dto LoginRequest :

---
+++ src/main/java/com/ga/YouCINEMA/dto/request/LoginRequest.java
@@ -0,0 +1,20 @@
+package com.ga.YouCINEMA.dto.request;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import lombok.*;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class LoginRequest {
+
+    @Email(message = "Email must be valid")
+    @NotBlank(message = "Email is required")
+    private String email;
+
+    @NotBlank(message = "Password is required")
+    private String password;
+}
