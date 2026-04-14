0923a8c010ff59d0056fdad5a5174cc83d95d0a4
yousif <yousif.sei07@gmail.com>
2026-04-14 19:19:47 +0300

    add auth controller

---
+++ src/main/java/com/ga/YouCINEMA/controller/AuthController.java
@@ -0,0 +1,32 @@
+package com.ga.YouCINEMA.controller;
+
+import com.ga.YouCINEMA.dto.request.LoginRequest;
+import com.ga.YouCINEMA.dto.request.RegisterRequest;
+
+import com.ga.YouCINEMA.dto.response.AuthenticatedUserResponse;
+import com.ga.YouCINEMA.service.AuthService;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+
+@RestController
+@RequestMapping("/auth")
+@RequiredArgsConstructor
+public class AuthController {
+
+    private final AuthService authService;
+
+
+    @PostMapping("/register")
+    public ResponseEntity<AuthenticatedUserResponse> register(@Valid @RequestBody RegisterRequest request) {
+        return ResponseEntity.ok(authService.register(request));
+    }
+
+
+    @PostMapping("/login")
+    public ResponseEntity<AuthenticatedUserResponse> login(@Valid @RequestBody LoginRequest request) {
+        return ResponseEntity.ok(authService.login(request));
+    }
+}
