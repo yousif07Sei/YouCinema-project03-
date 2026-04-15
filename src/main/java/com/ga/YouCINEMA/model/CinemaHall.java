689324ebffa9cb8d252d3b210a7b797908cbc4cd
yousif <yousif.sei07@gmail.com>
2026-04-15 11:22:47 +0300

    added the relationships and fix bugs by chaning some feilds type and some annations

--- src/main/java/com/ga/YouCINEMA/model/CinemaHall.java
+++ src/main/java/com/ga/YouCINEMA/model/CinemaHall.java
@@ -1,6 +1,10 @@
 package com.ga.YouCINEMA.model;
+import com.ga.YouCINEMA.enums.HallType;
+import jakarta.validation.constraints.NotBlank;
 import lombok.*;
 import jakarta.persistence.*;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
 
 
 import java.time.LocalDateTime;
@@ -15,18 +19,29 @@ import java.util.List;
 @Builder
 public class CinemaHall {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @NotBlank(message = "Hall name is required")
+    @Column(nullable = false)
     private String name;
 
-    private String hallType;
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private HallType hallType;
 
+    @Column(nullable = false)
     private int totalSeats;
 
+    @OneToMany(mappedBy = "cinemaHall", cascade = CascadeType.ALL, orphanRemoval = true)
     private List<Seat> seats;
 
+    @CreationTimestamp
+    @Column(updatable = false)
     private LocalDateTime createdAt;
 
+    @UpdateTimestamp
     private LocalDateTime updatedAt;
 
 }
--- src/main/java/com/ga/YouCINEMA/model/Showtime.java
+++ src/main/java/com/ga/YouCINEMA/model/Showtime.java
@@ -1,6 +1,8 @@
 package com.ga.YouCINEMA.model;
 import lombok.*;
 import jakarta.persistence.*;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
 
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
@@ -13,20 +15,32 @@ import java.time.LocalDateTime;
 @Builder
 public class Showtime {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @ManyToOne
+    @JoinColumn(name = "movie_id", nullable = false)
     private  Movie movie;
 
+    @ManyToOne
+    @JoinColumn(name = "hall_id", nullable = false)
     private CinemaHall cinemaHall;
 
+    @Column(nullable = false)
     private LocalDateTime startTime;
 
+    @Column(nullable = false)
     private LocalDateTime endTime;
 
+    @Column(nullable = false, precision = 10, scale = 2)
     private BigDecimal price;
 
+    @CreationTimestamp
+    @Column(updatable = false)
     private LocalDateTime createdAt;
 
+    @UpdateTimestamp
     private LocalDateTime updatedAt;
 
 }
