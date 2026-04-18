095b620d086f5e49d98e9a9654fc4685c4738039
yousif <yousif.sei07@gmail.com>
2026-04-18 23:59:36 +0300

    add MovieRespositry

---
+++ src/main/java/com/ga/YouCINEMA/repository/MovieRepository.java
@@ -0,0 +1,9 @@
+package com.ga.YouCINEMA.repository;
+
+import com.ga.YouCINEMA.model.Movie;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface MovieRepository extends JpaRepository<Movie, Long> {
+}
