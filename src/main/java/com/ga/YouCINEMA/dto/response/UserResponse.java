b64978bd8b7a039bdae7354dda0c10c612c329c4
yousif <yousif.sei07@gmail.com>
2026-04-18 17:50:43 +0300

    add User Service in service

--- src/main/java/com/ga/YouCINEMA/dto/response/UserResponse.java
+++ src/main/java/com/ga/YouCINEMA/dto/response/UserResponse.java
@@ -1,4 +1,20 @@
 package com.ga.YouCINEMA.dto.response;
 
+import lombok.*;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class UserResponse {
+
+    private Long id;
+    private String firstName;
+    private String lastName;
+    private String email;
+    private String role;
+    private String status;
+    private String profilePicture;
+    private boolean emailVerified;
 }
