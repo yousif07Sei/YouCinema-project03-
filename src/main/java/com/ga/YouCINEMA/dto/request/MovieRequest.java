c49ea94d94081a09ce38a16346538cc50445b2de
yousif <yousif.sei07@gmail.com>
2026-04-18 23:43:43 +0300

    add the MovieRequest in dto

---
+++ src/main/java/com/ga/YouCINEMA/dto/request/MovieRequest.java
@@ -0,0 +1,33 @@
+package com.ga.YouCINEMA.dto.request;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Positive;
+import lombok.*;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class MovieRequest {
+
+    @NotBlank(message = "Title is required")
+    private String title;
+
+    @NotBlank(message = "Description is required")
+    private String description;
+
+    @NotBlank(message = "Genre is required")
+    private String genre;
+
+    @NotBlank(message = "Language is required")
+    private String language;
+
+    @NotNull(message = "Duration is required")
+    @Positive(message = "Duration must be positive")
+    private Integer durationMinutes;
+
+    @NotBlank(message = "Director is required")
+    private String director;
+}
